5602184f80f5f9d441c4c7491c37c4918c5398e2
Your Name <you@example.com>
2023-02-22 00:47:39 +0000

    fix boostrap icons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import'./assets/styles/bootstrap.min.css'
 import './assets/styles/index.css';
 import './assets/styles/menu.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
